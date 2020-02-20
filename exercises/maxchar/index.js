baa01b1a3bc3cb9d363219c5b4289311b139dea5
amado-saladino <salaheldin.ahmed1@gmail.com>
2020-02-20 23:57:45 +0200

    prepare for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars ={}
+    for (ch of str) {
+        chars[ch] = !chars[ch] ? chars[ch] = 1 : chars[ch]++
+        //chars[ch] = chars[ch] + 1 || 1  //the same as line above
+    }
+}
 
-module.exports = maxChar;
+function maxChar1(str) {
+    const letters = new Map()
+    for (ch of str) {
+        letters.has(ch) ? letters.set(ch, letters.get(ch)+1 ) : letters.set(ch,1)
+    }
+    
+    let max = 0
+    let c = ''
+    for ([ch,count] of letters.entries()){
+        count > max ? (max = count, c= ch) : max
+    }
+    console.log(`Character: ${c}, repeated: ${max}`)
+    return c
+}
+
+//module.exports = maxChar;
+module.exports = {maxChar, maxChar1}
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,17 @@
-const maxChar = require('./index');
+const maxChar = require('./index').maxChar
+const maxChar1 = require('./index').maxChar1
 
-test('maxChar function exists', () => {
-  expect(typeof maxChar).toEqual('function');
-});
+describe('Using map', () =>{
+  test('maxChar function exists', () => {
+    expect(typeof maxChar1).toEqual('function');
+  });
 
-test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
-  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
-});
+  test('Finds the most frequently used char', () => {
+    expect(maxChar1('a')).toEqual('a');
+    expect(maxChar1('abcdefghijklmnaaaaa')).toEqual('a');
+  });
 
-test('Works with numbers in the string', () => {
-  expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
-});
+  test('Works with numbers in the string', () => {
+    expect(maxChar1('ab1c1d1e1f1g1')).toEqual('1');
+  })
+})
