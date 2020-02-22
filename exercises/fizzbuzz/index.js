d386375c1bcbd900cadd0949e8c052326e1d2ec5
amado-saladino <salaheldin.ahmed1@gmail.com>
2020-02-22 15:05:40 +0200

    fizzbuzz and maxChar

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,38 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 ===0)  console.log('fizzbuzz') 
+        else if (i % 3 === 0)  console.log('fizz') 
+        else if (i % 5 === 0)  console.log('buzz')
+        else console.log(i)
+    }
+ }
 
-module.exports = fizzBuzz;
+function fizzBuzz1(n) {
+    let i = 1
+    while (i <= n) {
+        if (i % 15 === 0)
+            console.log('fizzbuzz')
+        else if (i % 3 === 0)
+            console.log('fizz')
+        else if (i % 5 === 0)
+            console.log('buzz')
+        else
+            console.log(i)
+        i++
+    }
+}
+
+function fizzBuzz2(n) {
+    for (let i = 1; i <= n; i++) {
+        i % 15 === 0 ? (console.log('fizzbuzz') )
+        : i % 3 === 0 ? (console.log('fizz') )
+        : i % 5 === 0 ? (console.log('buzz')) 
+        : (console.log(i))
+    }
+}
+
+//module.exports = fizzBuzz;
+module.exports = { fizzBuzz, fizzBuzz1, fizzBuzz2 }
