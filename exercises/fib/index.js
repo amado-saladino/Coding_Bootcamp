ee54ba492b323b8fa5a9d8e176487da6de0f9aa3
amado-saladino <salaheldin.ahmed1@gmail.com>
2020-08-03 13:23:48 +0200

    fibonnaci, queue and stack

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,61 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib_recur(n) {
+    //or if (n < 2)
+    if (n <= 1) return n
+    return fib_recur(n - 1) + fib_recur(n - 2)
+}
 
-module.exports = fib;
+const fib_iter = (n)=> {
+    num1 = 0
+    num2 = 1
+    i = 0
+    while (i < n) {
+        tmp = num1
+        num1 = num2        
+        num2 = num1 + tmp
+        i++
+    }
+    return num1
+}
+
+const fib_iter_arr = (n)=> {
+    const res = [0, 1]
+    for (i=2; i<= n; i++){
+        res.push(res[i - 1] + res[i - 2])
+    }
+    return res[res.length - 1] //or res[n]
+}
+
+//*************************************** */
+function fib_recur_slow(n) {
+    //or if (n < 2)
+    if (n <= 1) return n
+    return fibi(n - 1) + fibi(n - 2)
+}
+
+function memoizer(func) {
+    const cache = {}
+    return function(...args) {
+        if (cache[args]) return cache[args]
+
+        const result = func(...args)
+        cache[args] = result
+        return result
+    }
+}
+
+const fibi = memoizer(fib_recur_slow)
+
+/***************************************** */
+function fibonacci_v2(n, mem) {
+    mem = mem || {}
+    if (mem[n]) return mem[n]
+
+    if (n <= 1) return n
+
+   return mem[n] = fibonacci_v2(n - 1, mem) + fibonacci_v2(n - 2, mem)
+}
+
+module.exports = {fib_recur, fib_iter, fib_iter_arr, fibi, fibonacci_v2}
--- exercises/fib/test.js
+++ exercises/fib/test.js
@@ -1,25 +1,135 @@
-const fib = require('./index');
+const fib = require('./index').fib_recur
+const fib1 = require('./index').fib_iter
+const fib2 = require('./index').fib_iter_arr
+const fib3 = require('./index').fibi
+const fib4 = require('./index').fibonacci_v2
 
-test('Fib function is defined', () => {
-  expect(typeof fib).toEqual('function');
-});
+describe('Fibonacci using recursive', ()=>{
+  test('Fib function is defined', () => {
+    expect(typeof fib).toEqual('function');
+  });
+  
+  test('calculates correct fib value for 1', () => {
+    expect(fib(1)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 2', () => {
+    expect(fib(2)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 3', () => {
+    expect(fib(3)).toEqual(2);
+  });
+  
+  test('calculates correct fib value for 4', () => {
+    expect(fib(4)).toEqual(3);
+  });
+  
+  test('calculates correct fib value for 15', () => {
+    expect(fib(39)).toEqual(63245986);
+  });
+})
 
-test('calculates correct fib value for 1', () => {
-  expect(fib(1)).toEqual(1);
-});
+describe('Fibonacci [iterative]', ()=>{
+  test('Fib function is defined', () => {
+    expect(typeof fib1).toEqual('function');
+  });
+  
+  test('calculates correct fib value for 1', () => {
+    expect(fib1(1)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 2', () => {
+    expect(fib1(2)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 3', () => {
+    expect(fib1(3)).toEqual(2);
+  });
+  
+  test('calculates correct fib value for 4', () => {
+    expect(fib1(4)).toEqual(3);
+  });
+  
+  test('calculates correct fib value for 15', () => {
+    expect(fib(39)).toEqual(63245986);
+  });
+})
 
-test('calculates correct fib value for 2', () => {
-  expect(fib(2)).toEqual(1);
-});
+describe('Fibonacci [iterative - array] ', ()=>{
+  test('Fib function is defined', () => {
+    expect(typeof fib2).toEqual('function');
+  });
+  
+  test('calculates correct fib value for 1', () => {
+    expect(fib2(1)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 2', () => {
+    expect(fib2(2)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 3', () => {
+    expect(fib2(3)).toEqual(2);
+  });
+  
+  test('calculates correct fib value for 4', () => {
+    expect(fib2(4)).toEqual(3);
+  });
+  
+  test('calculates correct fib value for 15', () => {
+    expect(fib(39)).toEqual(63245986);
+  });
+})
 
-test('calculates correct fib value for 3', () => {
-  expect(fib(3)).toEqual(2);
-});
+describe('Fibonacci [recursive - memoizer] ', ()=>{
+  test('Fib function is defined', () => {
+    expect(typeof fib3).toEqual('function');
+  });
+  
+  test('calculates correct fib value for 1', () => {
+    expect(fib3(1)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 2', () => {
+    expect(fib3(2)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 3', () => {
+    expect(fib3(3)).toEqual(2);
+  });
+  
+  test('calculates correct fib value for 4', () => {
+    expect(fib3(4)).toEqual(3);
+  });
+  
+  test('calculates correct fib value for 15', () => {
+    expect(fib3(39)).toEqual(63245986);
+  });
+})
 
-test('calculates correct fib value for 4', () => {
-  expect(fib(4)).toEqual(3);
-});
-
-test('calculates correct fib value for 15', () => {
-  expect(fib(39)).toEqual(63245986);
-});
+describe('Fibonacci [recursive - built in memoizer] ', ()=>{
+  test('Fib function is defined', () => {
+    expect(typeof fib4).toEqual('function');
+  });
+  
+  test('calculates correct fib value for 1', () => {
+    expect(fib4(1)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 2', () => {
+    expect(fib4(2)).toEqual(1);
+  });
+  
+  test('calculates correct fib value for 3', () => {
+    expect(fib4(3)).toEqual(2);
+  });
+  
+  test('calculates correct fib value for 4', () => {
+    expect(fib4(4)).toEqual(3);
+  });
+  
+  test('calculates correct fib value for 15', () => {
+    expect(fib4(39)).toEqual(63245986);
+  });
+})
